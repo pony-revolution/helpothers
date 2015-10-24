eb6d431a72061ae44b8411731b0eb3f36a2a3c43
Loek van Gent <loek@1procentclub.nl>
2015-10-24 12:47:28 +0200

    CHanges

--- helpothers/settings.py
+++ helpothers/settings.py
@@ -112,6 +112,8 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
+# AUTH_USER_MODEL = 'helpothers.User'
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,10 +121,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+
+    'helpothers'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
