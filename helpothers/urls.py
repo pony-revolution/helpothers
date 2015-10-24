8166319b02c9e02a6560af46c46217fee41ec699
Loek van Gent <loek@1procentclub.nl>
2015-10-24 21:25:38 +0200

    First changes for i18n

--- helpothers/urls.py
+++ helpothers/urls.py
@@ -1,12 +1,16 @@
-from django.conf.urls import patterns, include, url
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url, patterns
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from helpothers.views import HomeView
+
+from helpothers.views import HomeView, ProfileView
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', HomeView.as_view(), name='home'),
 
+urlpatterns = patterns(
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'accounts/profile', ProfileView.as_view(), name='account-profile'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
 
