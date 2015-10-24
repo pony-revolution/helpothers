1d15f368c06df5d6a13372a5bdc07005a5886e9b
Zan Anderle <zan.anderle@gmail.com>
2015-10-25 01:03:23 +0200

    Add basic views and templates

--- helpothers/urls.py
+++ helpothers/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls import include, url, patterns
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-from helpothers.views import HomeView, ProfileView
+from helpothers.views import HomeView, ProfileView, GatheringCenterView, ResourceView
 
 admin.autodiscover()
 
 
 urlpatterns = i18n_patterns(
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^center/(?P<pk>\d+)$', GatheringCenterView.as_view(), name='gathering_center'),
+    url(r'^resource/(?P<pk>\d+)$', ResourceView.as_view(), name='resource'),
     url(r'accounts/profile', ProfileView.as_view(), name='account-profile'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
