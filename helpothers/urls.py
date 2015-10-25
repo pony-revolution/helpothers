b9fe6e5eab8e4f90ed6fa45a2cc90945e7969cbe
Loek van Gent <loek@1procentclub.nl>
2015-10-25 21:19:19 +0100

    Add resource-add page and some other logic

--- helpothers/urls.py
+++ helpothers/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import include, url, patterns
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-from helpothers.views import HomeView, ProfileView, GatheringCenterView, ResourceView
+from helpothers.views import (HomeView, ProfileView, GatheringCenterView,
+                              ResourceCreateView, ResourceDetailView, ReviewView)
 
 admin.autodiscover()
 
@@ -11,7 +12,11 @@ admin.autodiscover()
 urlpatterns = i18n_patterns(
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^center/(?P<pk>\d+)$', GatheringCenterView.as_view(), name='gathering_center'),
-    url(r'^resource/(?P<pk>\d+)$', ResourceView.as_view(), name='resource'),
+
+    url(r'^resource/(?P<pk>\d+)$', ResourceDetailView.as_view(), name='resource'),
+    url(r'^resource/add$', ResourceCreateView.as_view(), name='resource-add'),
+    url(r'^resource/review$', ReviewView.as_view(), name='resource-review'),
+
     url(r'accounts/profile', ProfileView.as_view(), name='account-profile'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
