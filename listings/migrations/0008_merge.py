5e55bad7d416c13e5ed969fee14125a9b8de9cb0
Loek van Gent <loek@1procentclub.nl>
2015-10-31 10:05:37 +0100

    Add merge migration

---
+++ listings/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('listings', '0007_auto_20151030_2013'),
+        ('listings', '0005_auto_20151030_2003'),
+    ]
+
+    operations = [
+    ]
